92d895392ca2687d54a05ab68b2192b130518395
Sasha Manyonok <83176347+EriccCarmann@users.noreply.github.com>
2025-04-03 10:25:35 +0300

    added error messages to login screen

--- LoginTestAppMaui/LoginTestAppMaui/ViewModels/LoginViewModel.cs
+++ LoginTestAppMaui/LoginTestAppMaui/ViewModels/LoginViewModel.cs
@@ -19,31 +19,54 @@ namespace LoginTestAppMaui.ViewModels
         [ObservableProperty]
         private string password;
 
+        [ObservableProperty]
+        private bool errorMessageVisibility;
+        
+        [ObservableProperty]
+        private string errorMessageText;
+
+        public IRelayCommand CloseErrorMessage { get; }
+
         private readonly IPreferencesService _preferences;
+
+        private string matchErrorMessage = "The information you entered does not match our record.";
+        private string emptyFieldsErrorMessage = "Make sure that all of the fields are not empty.";
 
         public LoginViewModel(IPreferencesService preferences)
         {
             LoginCommand = new RelayCommand(OnLoginCommand);
+            CloseErrorMessage = new RelayCommand(OnCloseErrorMessage);
             _preferences = preferences;
+
+            ErrorMessageVisibility = false;
         }
 
         private void OnLoginCommand()
         {
-            if (string.IsNullOrEmpty(login) ||
-                string.IsNullOrEmpty(email) ||
-                string.IsNullOrEmpty(password))
+            if (string.IsNullOrEmpty(Login) ||
+                string.IsNullOrEmpty(Email) ||
+                string.IsNullOrEmpty(Password))
             {
-                CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-
-                var toast = Toast.Make("Fill all fields!", ToastDuration.Short, 15);
-
-                toast.Show(cancellationTokenSource.Token);
+                ErrorMessageVisibility = true;
+                ErrorMessageText = emptyFieldsErrorMessage;
+                
+            }
+            else if (!Login.Equals("Alex"))
+            {
+                ErrorMessageVisibility = true;
+                ErrorMessageText = matchErrorMessage;
             }
             else
             {
-                _preferences.SetStringPreference(login);
+                ErrorMessageVisibility = false;
+                _preferences.SetStringPreference(Login);
                 Shell.Current.GoToAsync("MainPage");
             }
+        }
+
+        private void OnCloseErrorMessage()
+        {
+            ErrorMessageVisibility = false;
         }
     }
 }
