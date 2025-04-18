8caa0829ee3699a518b8f7d2a9318117cbd30e51
Sasha Manyonok <83176347+EriccCarmann@users.noreply.github.com>
2025-04-18 09:53:29 +0300

    added multiple selection

--- LoginTestAppMaui/LoginTestAppMaui/Views/MainPage.xaml.cs
+++ LoginTestAppMaui/LoginTestAppMaui/Views/MainPage.xaml.cs
@@ -11,6 +11,27 @@ public partial class MainPage : ContentPage
         InitializeComponent();
 
         BindingContext = viewModel;
+        UpdateSelectionData(Enumerable.Empty<string>());
+    }
+
+    void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        UpdateSelectionData(e.CurrentSelection);
+    }
+
+    void UpdateSelectionData(IEnumerable<object> currentSelectedItems)
+    {
+        var current = ToList(currentSelectedItems);
+        currentSelectedItemLabel.Text = string.IsNullOrWhiteSpace(current) ? "[none]" : current;
+    }
+        static string ToList(IEnumerable<object> items)
+    {
+        if (items == null)
+        {
+            return string.Empty;
+        }
+
+        return items.Aggregate(string.Empty, (sender, obj) => sender + (sender.Length == 0 ? "" : ", ") + ((string)obj));
     }
 
     protected override void OnNavigatedTo(NavigatedToEventArgs args)
